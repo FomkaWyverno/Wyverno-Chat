98ec414d02ace779edcf6806fd74e311807a0ddc
Fomka_Wyverno <kostiaklimishagghh@gmail.com>
2023-03-04 18:02:59 +0200

    Add message listener in java program

--- java/OverlayTwitchAPI/src/main/java/ua/wyverno/Main.java
+++ java/OverlayTwitchAPI/src/main/java/ua/wyverno/Main.java
@@ -2,6 +2,11 @@ package ua.wyverno;
 
 import com.github.twitch4j.TwitchClient;
 import com.github.twitch4j.TwitchClientBuilder;
+import com.github.twitch4j.chat.events.channel.IRCMessageEvent;
+import com.github.twitch4j.common.events.domain.EventUser;
+import com.github.twitch4j.helix.domain.ChatBadge;
+
+import java.util.Optional;
 
 public class Main {
 
@@ -9,5 +14,16 @@ public class Main {
         TwitchClient twitchClient = TwitchClientBuilder.builder()
                                                         .withEnableChat(true)
                                                         .build();
+
+        twitchClient.getChat().joinChannel("Fomka_Wyverno");
+
+
+        twitchClient.getEventManager().onEvent(IRCMessageEvent.class, event -> {
+            Optional<String> optionalMessage = event.getMessage();
+            Optional<String> optionalClientName = event.getClientName();
+            if (optionalClientName.isPresent()&&optionalMessage.isPresent()) {
+                System.out.printf("%s: %s%n",optionalClientName.get(),optionalMessage.get());
+            }
+        });
     }
 }
