fb6a7dc7fddad491ec40bb8f0448480e4ec96f09
Fomka_Wyverno <kostiaklimishagghh@gmail.com>
2023-03-08 12:04:29 +0200

    Method main handles exception

--- java/OverlayTwitchAPI/src/main/java/ua/wyverno/Main.java
+++ java/OverlayTwitchAPI/src/main/java/ua/wyverno/Main.java
@@ -3,13 +3,18 @@ package ua.wyverno;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ua.wyverno.twitch.api.authorization.Authorization;
+import ua.wyverno.util.ExceptionToString;
 
 public class Main {
 
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
-        logger.info("Start main class");
-        new Authorization();
+        try {
+            logger.info("Start main class");
+            new Authorization();
+        } catch (Exception e) {
+            logger.error(ExceptionToString.getString(e));
+        }
     }
 }
