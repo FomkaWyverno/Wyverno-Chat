44ddec6468fb460df871772cf7c6361bf9e3b8f5
Fomka_Wyverno <kostiaklimishagghh@gmail.com>
2023-03-08 17:10:11 +0200

    Handler for main page

---
+++ java/OverlayTwitchAPI/src/main/java/ua/wyverno/twitch/api/http/server/handlers/MainHandle.java
@@ -0,0 +1,35 @@
+package ua.wyverno.twitch.api.http.server.handlers;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+
+public class MainHandle implements HttpHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(MainHandle.class);
+    @Override
+    public void handle(HttpExchange t) throws IOException {
+        logger.debug("Main page GET");
+        File page = new File("htmls/main/index.html");
+        byte[] pageBytes = Files.readAllBytes(page.toPath());
+        logger.debug("Reading all bytes for Page.");
+
+        String response = new String(pageBytes, StandardCharsets.UTF_8);
+
+        t.sendResponseHeaders(200,response.length());
+        t.getResponseHeaders().add("Content-Type","text/html; charset=UTF-8");
+
+        OutputStream os = t.getResponseBody();
+
+        os.write(response.getBytes());
+        os.close();
+        logger.debug("END Main page GET");
+    }
+}
