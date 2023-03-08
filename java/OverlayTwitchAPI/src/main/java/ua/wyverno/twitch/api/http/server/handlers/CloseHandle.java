46a93bda22e667646d962763801fd3e327be25e2
Fomka_Wyverno <kostiaklimishagghh@gmail.com>
2023-03-08 15:36:11 +0200

    Create CloseHandle for close program

---
+++ java/OverlayTwitchAPI/src/main/java/ua/wyverno/twitch/api/http/server/handlers/CloseHandle.java
@@ -0,0 +1,14 @@
+package ua.wyverno.twitch.api.http.server.handlers;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+
+public class CloseHandle implements HttpHandler {
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+
+    }
+}
