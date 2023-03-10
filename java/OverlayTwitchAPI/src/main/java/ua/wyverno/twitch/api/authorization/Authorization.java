cdf507f4cc87953e4abc490de0f089f49be2ef78
Fomka_Wyverno <kostiaklimishagghh@gmail.com>
2023-03-11 00:16:00 +0200

    OAuth2Credential migrate to Account

--- java/OverlayTwitchAPI/src/main/java/ua/wyverno/twitch/api/authorization/Authorization.java
+++ java/OverlayTwitchAPI/src/main/java/ua/wyverno/twitch/api/authorization/Authorization.java
@@ -4,20 +4,10 @@ import com.github.philippheuer.credentialmanager.domain.OAuth2Credential;
 import com.github.twitch4j.auth.providers.TwitchIdentityProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import ua.wyverno.twitch.api.http.server.HttpServer;
-import ua.wyverno.util.ExceptionToString;
-
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Properties;
 
 public class Authorization {
 
     private static final Logger logger = LoggerFactory.getLogger(Authorization.class);
-
-    private OAuth2Credential auth2Credential;
     private final String accessToken;
 
     public Authorization(String accessToken) throws AccessTokenNoLongerValidException {
@@ -34,10 +24,5 @@ public class Authorization {
                 .orElse(false);
         logger.debug("Credential is valid -> " + isValidToken);
         return isValidToken;
-    }
-
-    public OAuth2Credential getAccount() {
-        if (this.auth2Credential == null) this.auth2Credential = new OAuth2Credential("twitch", this.accessToken);
-        return this.auth2Credential;
     }
 }
