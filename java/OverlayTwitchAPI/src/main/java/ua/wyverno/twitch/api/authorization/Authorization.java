9c5d2cecc46e3843ad05f99475d1e9f38cb9aa4f
Fomka_Wyverno <kostiaklimishagghh@gmail.com>
2023-03-11 19:30:28 +0200

    Register Account and GetInstanceAccount

--- java/OverlayTwitchAPI/src/main/java/ua/wyverno/twitch/api/authorization/Authorization.java
+++ java/OverlayTwitchAPI/src/main/java/ua/wyverno/twitch/api/authorization/Authorization.java
@@ -10,6 +10,8 @@ public class Authorization {
     private static final Logger logger = LoggerFactory.getLogger(Authorization.class);
     private final String accessToken;
 
+    private static Account instance;
+
     public Authorization(String accessToken) throws AccessTokenNoLongerValidException {
         this.accessToken = accessToken;
         if (accessToken == null || accessToken.isEmpty() || !isValidToken(this.accessToken)) {
@@ -24,5 +26,29 @@ public class Authorization {
                 .orElse(false);
         logger.debug("Credential is valid -> " + isValidToken);
         return isValidToken;
+    }
+
+    public static Account registerAccount(String accessToken) throws AccessTokenNoLongerValidException {
+        logger.info("Register new Account!");
+
+        logger.debug("Check validate access token");
+
+        if (!isValidToken(accessToken)) {
+            logger.warn("Access Token not valid! Registration failed!");
+            throw new AccessTokenNoLongerValidException();
+        }
+
+        instance = new Account(accessToken, ConfigHandler.getInstance().getClientID());
+
+        return instance;
+    }
+
+    public static Account getAccountInstance() throws AccessTokenNoLongerValidException {
+        logger.info("Get Account Instance | instance == null? -> " + (instance == null));
+        if (instance != null) return instance;
+
+        logger.warn("Account Instance == null!!");
+        logger.warn("Try register account!");
+        return registerAccount(ConfigHandler.getInstance().getAccessToken());
     }
 }
