f79c6650f7945c101e155150dd12dec17fb9583b
Fomka_Wyverno <kostiaklimishagghh@gmail.com>
2023-03-11 22:50:01 +0200

    Protocol for WebSocket

---
+++ java/OverlayTwitchAPI/src/main/java/ua/wyverno/twitch/api/chat/Protocol.java
@@ -0,0 +1,35 @@
+package ua.wyverno.twitch.api.chat;
+
+public class Protocol {
+
+    public TYPE type;
+
+    public String content;
+
+    public Protocol() {}
+
+    public Protocol(TYPE type, String content) {
+        this.type = type;
+        this.content = content;
+    }
+
+    public TYPE getType() {
+        return type;
+    }
+
+    public void setType(TYPE type) {
+        this.type = type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public static enum TYPE {
+        text,html,number_spectators
+    }
+}
