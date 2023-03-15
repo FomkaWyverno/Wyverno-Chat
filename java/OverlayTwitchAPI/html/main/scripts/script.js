66a587e59b2af53da5f56bee3706e748c581f895
Fomka_Wyverno <kostiaklimishagghh@gmail.com>
2023-03-15 18:16:32 +0200

    Hover partical effect

--- java/OverlayTwitchAPI/html/main/scripts/script.js
+++ java/OverlayTwitchAPI/html/main/scripts/script.js
@@ -2,7 +2,13 @@ const { ipcRenderer } = require('electron');
 
 const loadingSpan = document.querySelector('.loading')
 const loginButton = document.querySelector('.login');
+const containerLogged = document.querySelector('.container__logged')
 const openOverlay = document.querySelector('.open-overlay');
+
+// Description Block
+const channel_logo = document.querySelector('.channel-description__logo')
+const channel_username = document.querySelector('.channel-description__username');
+const channel_followers = document.querySelector('.channel-description__followers');
 
 //loggin();
 
@@ -28,13 +34,15 @@ function loggin() {
         if (xhr.readyState === XMLHttpRequest.DONE) {
             if (xhr.status === 200) {
                 console.log('AccessToken is valid!')
-                const html = xhr.responseText;
-                console.log(html);
-                loginButton.classList.add('hide')
-                openOverlay.classList.remove('hide')
+                const jsonResponse = JSON.parse(xhr.responseText);
+                console.log(jsonResponse);
+                loginButton.classList.add('hide') // Відключаємо кнопку авторизації
+                loadingSpan.classList.add('hide') // Вимикаємо загрузку
 
-
-                loadingSpan.classList.add('hide')
+                containerLogged.classList.remove('hide') // Відображуємо авторизоване вікно
+                channel_logo.src = jsonResponse.profileImageURL;
+                channel_username.textContent = jsonResponse.displayName;
+                channel_followers.textContent = `${jsonResponse.countFollowers} followers`
             } else if (xhr.status === 401) {
                 console.log('AccessToken is not valid!')
                 loginButton.classList.remove('hide')
@@ -68,7 +76,7 @@ function authorization() {
 
     xhr.send();
 
-    if(!isRunWaitAuth) {
+    if (!isRunWaitAuth) {
         isRunWaitAuth = true;
         wait_authorization();
     }
@@ -78,17 +86,17 @@ function authorization() {
 let isRunWaitAuth = false;
 
 function wait_authorization() {
-        fetch('/authorization-status')
-            .then(r => r.json())
-            .then(isAuth => {
-                if (isAuth) {
-                    isRunWaitAuth = false;
-                    loggin();
-                } else {
-                    setTimeout(wait_authorization, 2000);
-                }
-            }).catch(error => {
-                console.error(`Помилка: ${error}`)
-            });
-    
+    fetch('/authorization-status')
+        .then(r => r.json())
+        .then(isAuth => {
+            if (isAuth) {
+                isRunWaitAuth = false;
+                loggin();
+            } else {
+                setTimeout(wait_authorization, 2000);
+            }
+        }).catch(error => {
+            console.error(`Помилка: ${error}`)
+        });
+
 }
